0152fbf09cf3c2fe9f4974833e646e6f6b284b10
AllaByshkina <alla.byshkin@gmail.com>
2021-04-28 18:48:01 +0300

    Solution

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -12,7 +12,46 @@
  * @return {object}
  */
 function convertToObject(sourceString) {
-  // write your code here
+  if (sourceString !== undefined) {
+    const newAr = [];
+    const key = [];
+    const value = [];
+    let rest;
+    let count;
+    const values = [];
+    const keys = [];
+    const arr = sourceString.split(';');
+
+    for (let i = 0; i < arr.length; i++) {
+      if (arr[i] !== '') {
+        newAr[newAr.length] = arr[i].trim().split(':');
+      }
+    }
+
+    for (let i = 0; i < newAr.length; i++) {
+      if (typeof newAr[i] === 'object') {
+        [count, ...rest] = newAr[i];
+        value[value.length] = rest.join('');
+        key[key.length] = count;
+      };
+    };
+
+    for (let i = 0; i < value.length; i++) {
+      if (value[i] !== '') {
+        values[values.length] = value[i].trim();
+      }
+    };
+
+    for (let i = 0; i < key.length; i++) {
+      if (key[i] !== '') {
+        keys[keys.length] = key[i].trim();
+      }
+    };
+
+    const result = Object.assign(...keys.map((k, i) => ({ [k]: values[i] })));
+
+    return result;
+  }
 }
 
 module.exports = convertToObject;
